f567fdc008138d1ec7d3c93c9906456489066b9b
lifeOnHack <ilayl9991@gmail.com>
2025-07-07 16:10:00 +0300

    add okta api

---
+++ okta_api.py
@@ -0,0 +1,29 @@
+import os, requests
+from typing import List, Dict
+
+OKTA_API_URL = f"https://{os.getenv('OKTA_DOMAIN')}/api/v1"
+
+
+def get_okta_headers() -> Dict[str, str]:
+    """
+    Returns headers for Okta API requests.
+    """
+    return {
+        "Authorization": f"SSWS {os.getenv('OKTA_API_TOKEN')}",
+        "Accept": "application/json"
+    }
+
+
+def get_okta_users() -> List[Dict]:
+    """
+    Fetch all users in the Okta org.
+    """
+    url = f"{OKTA_API_URL}/users"
+    response = requests.get(url, headers=get_okta_headers())
+    response.raise_for_status()
+    return response.json()
+
+if __name__ == "__main__":
+    users = get_okta_users()
+    print(f"✅ Found {len(users)} users in Okta\n\n")
+    print(users)
